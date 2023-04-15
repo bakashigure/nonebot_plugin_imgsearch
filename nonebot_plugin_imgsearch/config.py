db3813adfdda8247fc6e95dfc2ba70258d8ae3af
RinChanNOWWW <rinchannow@bupt.edu.cn>
2023-04-15 10:47:45 +0800

    More robust.

--- nonebot_plugin_imgsearch/config.py
+++ nonebot_plugin_imgsearch/config.py
@@ -3,8 +3,5 @@ from nonebot import get_driver
 
 config_dict = get_driver().config.dict()
 
-search_proxy = None
-if 'search_proxy' in config_dict:
-    search_proxy = config_dict['saucenao_api_key']
-
-saucenao_api_key = config_dict['saucenao_api_key']
+search_proxy = config_dict.get('search_proxy')
+saucenao_api_key = config_dict.get('saucenao_api_key')
